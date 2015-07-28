3ac29a649c31522f5e45942eb2f23a90da577e80
Kobus Coetzee <kobusengineer@gmail.com>
2015-07-28 14:22:33 +0200

    Ok two submodules was still hiding, removed and commiting their source now

---
+++ submodules/linphone/mediastreamer2/tester/mediastreamer2_tester_windows.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "mediastreamer2/mscommon.h"
+#include "mediastreamer2_tester.h"
+
+namespace ms2_tester_runtime_component
+{
+	public interface class OutputTraceListener
+	{
+	public:
+		void outputTrace(Platform::String^ lev, Platform::String^ msg);
+	};
+
+    public ref class MS2Tester sealed
+    {
+    public:
+		void setOutputTraceListener(OutputTraceListener^ traceListener);
+		unsigned int nbTestSuites();
+		unsigned int nbTests(Platform::String^ suiteName);
+		Platform::String^ testSuiteName(int index);
+		Platform::String^ testName(Platform::String^ suiteName, int testIndex);
+		bool run(Platform::String^ suiteName, Platform::String^ caseName, Platform::Boolean verbose);
+		void runAllToXml();
+		void startVideoStream(Platform::Object^ CaptureElement, Platform::Object^ MediaElement);
+		void stopVideoStream();
+
+		static property MS2Tester^ Instance
+		{
+			MS2Tester^ get() { return _instance; }
+		}
+		property Windows::Foundation::IAsyncAction^ AsyncAction
+		{
+			Windows::Foundation::IAsyncAction^ get() { return _asyncAction; }
+		}
+	private:
+		MS2Tester();
+		~MS2Tester();
+		void init(bool verbose);
+
+		static MS2Tester^ _instance;
+		Windows::Foundation::IAsyncAction^ _asyncAction;
+		VideoStream *_videoStream;
+	};
+}
