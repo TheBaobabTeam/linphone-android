3fbf654dcdc249cdb99bb295d0d3921ce1665e68
patiencemtsweni <u11116774@tuks.co.za>
2015-08-23 01:47:58 +0200

    Created tables for results in document

--- svr_generation_script.py
+++ svr_generation_script.py
@@ -5,10 +5,26 @@ document = Document()
 
 document.add_heading('Unit Testing Results Document', 0)
 
-table = document.add_table(rows=5, cols=4)
+details_table = document.add_table(rows=5, cols=4)
 
-p_name = table.cell(0,0).text = 'Project Name: '
+p_name = details_table.cell(0,0).text = 'Project Name: '
+version = details_table.cell(0,2).text = 'Version:' 
+author = details_table.cell(1,0).text = 'Written by: '
+desc = details_table.cell(1,2).text = 'Description '
 
+document.add_heading('Table showing results', level=2)
+
+test_table = document.add_table(rows=5, cols=7)
+
+# populate header row --------
+heading_cells = test_table.rows[0].cells
+heading_cells[0].text = '#'
+heading_cells[1].text = 'Date Added'
+heading_cells[2].text = 'Use case'
+heading_cells[3].text = 'Expected Result'
+heading_cells[4].text = 'Passed?'
+heading_cells[5].text = 'Failed?'
+heading_cells[6].text = 'Remarks'
 
 document.save('Acceptance_Test_Results.docx')
 
