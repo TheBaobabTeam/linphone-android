4de17032be7761dcc3d9b74ab7675fae7e769c2f
patiencemtsweni <u11116774@tuks.co.za>
2015-10-15 11:15:07 +0200

    Updated LinphoneActivity.java

--- src/org/linphone/LinphoneActivity.java
+++ src/org/linphone/LinphoneActivity.java
@@ -19,14 +19,12 @@ package org.linphone;
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 import static android.content.Intent.ACTION_MAIN;
-
+import org.linphone.groupchat.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.List;
-
-import java.util.Arrays;
 
 import org.linphone.LinphoneManager.AddressType;
 import org.linphone.compatibility.Compatibility;
@@ -49,6 +47,9 @@ import org.linphone.groupchat.AddMembersActivity;
 import org.linphone.groupchat.FragmentsAvailable;
 import org.linphone.groupchat.GroupChatRoomActivity;
 import org.linphone.groupchat.NewGroupActivity;
+import org.linphone.groupchat.GroupDetailsActivity;
+import org.linphone.groupchat.WelcomeActivity;
+import org.linphone.groupchat.aboutActivity;
 import org.linphone.mediastream.Log;
 import org.linphone.setup.RemoteProvisioningLoginActivity;
 import org.linphone.setup.SetupActivity;
@@ -109,12 +110,13 @@ public class LinphoneActivity extends FragmentActivity implements OnClickListene
 	private List<FragmentsAvailable> fragmentsHistory;
 	private Fragment dialerFragment, messageListFragment, friendStatusListenerFragment;
 	private ChatFragment chatFragment;
+	private GroupChatRoomFragment gcFragment;
 	private SavedState dialerSavedState;
 	private boolean isAnimationDisabled = false, preferLinphoneContacts = false;
 	private OrientationEventListener mOrientationHelper;
 	private LinphoneCoreListenerBase mListener;
 
-	static final boolean isInstanciated() {
+	public static final boolean isInstanciated() {
 		return instance != null;
 	}
 
@@ -687,81 +689,44 @@ public class LinphoneActivity extends FragmentActivity implements OnClickListene
 	 * added by me, newGroup button
 	 */
 	public void newGroup() {
-		Intent intent = new Intent(this, NewGroupActivity.class);
+		Intent intent = new Intent(this, WelcomeActivity.class);
+		//Testing the group detail Activity GroupDetailsAcivity
+		//Intent intent = new Intent(this, GroupDetailsActivity.class);
 		
+		startOrientationSensor();
+		startActivityForResult(intent, CHAT_ACTIVITY);
+	}
+	public void nextScreenAfterWelcome(){
+		Intent intent = new Intent(this,NewGroupActivity.class);
+		startOrientationSensor();
+		startActivityForResult(intent, CHAT_ACTIVITY);
+	}
+	public void aboutScreen(){
+		Intent intent = new Intent(this,aboutActivity.class);
 		startOrientationSensor();
 		startActivityForResult(intent, CHAT_ACTIVITY);
 	}
 	
 	//Select members for group chat
-	public void addMembers(String groupName) {
+	public void addMembers() {
 		Intent intent = new Intent(this, AddMembersActivity.class);
-		intent.putExtra("GroupName", groupName);
+		//intent.putExtra("GroupName", groupName);
 		
 		startOrientationSensor();
 		startActivityForResult(intent, CHAT_ACTIVITY);
 	}
-	
-	public void goToChatList() {
-		changeCurrentFragment(FragmentsAvailable.CHATLIST, null);
-		chat.setSelected(true);
-	}
 
 	//Added by me
-		public void createGroupChat(String groupName, String [] groupMembers, int groupSize){
-			//Intent intent = new Intent(this, ChatActivity.class);
-			//intent.putExtra("GroupName", groupName);
-			//intent.putExtra("GroupMembers", groupMembers);
-			//intent.putExtra("GroupSize", groupSize);
-			//intent.putExtra("ChatType", 1);
+		public void createGroupChat(){
+			Intent intent = new Intent(this, GroupChatRoomActivity.class);
+			/*intent.putExtra("SipUri", sipUri);
+			if (contact != null) {
+				intent.putExtra("DisplayName", contact.getName());
+				intent.putExtra("PictureUri", pictureUri);
+				intent.putExtra("ThumbnailUri", thumbnailUri);*/
 			
-			LinphoneCore lc = LinphoneManager.getLcIfManagerNotDestroyedOrNull();
-			if (lc != null) {
-				LinphoneChatRoom chatRoom = null;
-				chatRoom = lc.getOrCreateGroupChatRoom(groupName, groupMembers, groupSize, 0, 0);
-				
-				if (chatRoom != null) {
-					LinphoneAddress la = chatRoom.getPeerAddress();
-					
-					String sipUri = la.asStringUriOnly();
-					String displayName = la.getDisplayName();
-					
-					//intent.putExtra("SipUri", sipUri);
-					//intent.putExtra("DisplayName", displayName);
-					
-					LinphoneProxyConfig pc = lc.getDefaultProxyConfig();
-					String identity = pc.getIdentity();
-					
-					String id = "";
-					try {
-						la = LinphoneCoreFactory.instance().createLinphoneAddress(identity);
-						id = la.getUserName();
-					} catch (LinphoneCoreException e) {
-						Log.e("Cannot display chat",e);
-						//return;
-					}
-					
-					String message = id + " created group " + groupName;
-					chatRoom.sendGroupMessage(message);
-					
-					onMessageSent(sipUri, message);
-					
-					//goToChatList();
-					
-					//displayCustomToast("Simunye grooves: " + sipUri, Toast.LENGTH_SHORT);
-				}
-				
-				//LinphoneProxyConfig pc = lc.getDefaultProxyConfig();
-				//String identity = pc.getIdentity();
-				//displayCustomToast("I am: " + identity, Toast.LENGTH_SHORT);
-			}
-			
-			/*startOrientationSensor();
+			startOrientationSensor();
 			startActivityForResult(intent, CHAT_ACTIVITY);
-			
-			LinphoneService.instance().resetMessageNotifCount();
-			LinphoneService.instance().removeMessageNotification();
-			displayMissedChats(getChatStorage().getUnreadMessageCount());*/
 		}
 		
 	@Override
@@ -852,6 +817,10 @@ public class LinphoneActivity extends FragmentActivity implements OnClickListene
 
 	public void updateChatFragment(ChatFragment fragment) {
 		chatFragment = fragment;
+	}
+	
+	public void updateGroupChatRoomFragment(GroupChatRoomFragment fragment) {
+		gcFragment = fragment;
 	}
 
 	public void updateChatListFragment(ChatListFragment fragment) {
